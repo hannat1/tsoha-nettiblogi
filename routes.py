0b1df94e6f9e397f5b7ba0f33a95cf1ad552b6b2
Hanna Turunen <hannamatilda.turunen@gmail.com>
2024-08-06 15:10:53 +0300

    Create routes for app

---
+++ routes.py
@@ -0,0 +1,59 @@
+from app import app
+from flask import render_template, request, redirect
+import posts, users
+
+@app.route("/")
+def index():
+    list = posts.get_list()
+    return render_template("index.html", posts=list)
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+    title = request.form["title"]
+    content = request.form["content"]
+    if posts.send(title, content):
+        return redirect("/")
+    else:
+        return render_template("error.html", message="Viestin lähetys ei onnistunut")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Rekisteröinti ei onnistunut")
+        
+@app.route("/viewpost/<int:post_id>")
+def view_post(post_id):
+    list = posts.selectpost(post_id)
+    print(list)
+    return render_template("/post.html", post=list)
