f2b6729447551ffac0b822d6ee5cec5ca483cb98
Hanna Turunen <hannamatilda.turunen@gmail.com>
2024-08-16 15:12:29 +0300

    SQL queries for setting visibility on comment and post for deleting them from app view, also queries for filteration by following and likes

--- posts.py
+++ posts.py
@@ -8,6 +8,11 @@ def get_list():
     result = db.session.execute(sql)
     return result.fetchall()
 
+def username(user_id):
+    sql = text("SELECT U.username FROM users U WHERE U.id=:user_id")
+    result = db.session.execute(sql, {"user_id":user_id})
+    return result.fetchone()
+
 def send(title, content):
     #Make new post
     user_id = users.user_id()
@@ -70,6 +75,16 @@ def like(post):
     db.session.execute(sql, {"post":post, "user":user_id})
     db.session.commit()
     return True
+
+def unlike(post_id):
+    #Unlike a post
+    print(post_id)
+    user_id = users.user_id()
+    sql = text("UPDATE likes SET visible=FALSE WHERE post_id=:post_id AND user_id=:user_id")
+    db.session.execute(sql, {"post_id":post_id, "user_id":user_id})
+    db.session.commit()
+    return True
+
     
 def likers_names(post):
     sql = text("SELECT U.username FROM users U, likes L WHERE L.user_id=U.id AND L.post_id=:post AND L.visible=TRUE")
@@ -91,8 +106,33 @@ def comment(comment, post_id):
     db.session.commit()
     return True
 
+def delete_comment(comment_id):
+    #Delete a comment
+    sql = text("UPDATE comments SET visible=FALSE WHERE id=:comment_id")
+    db.session.execute(sql, {"comment_id":comment_id})
+    db.session.commit()
+    return True
+
 def comments(post_id):
     # Get comments of a post
-    sql = text("SELECT C.content, C.user_id, U.username, C.sent_at FROM comments C, posts P, users U WHERE C.post_id=P.id AND C.post_id=:post_id AND C.user_id=U.id AND C.visible=TRUE ORDER BY C.sent_at DESC")
+    sql = text("SELECT C.content, C.user_id, U.username, C.sent_at, C.id, P.id FROM comments C, posts P, users U WHERE C.post_id=P.id AND C.post_id=:post_id AND C.user_id=U.id AND C.visible=TRUE ORDER BY C.sent_at DESC")
     result = db.session.execute(sql, {"post_id":post_id})
+    return result.fetchall()
+
+def delete_post(post_id):
+    sql = text("UPDATE posts SET visible=FALSE WHERE id=:post_id")
+    db.session.execute(sql, {"post_id":post_id})
+    db.session.commit()
+    return True
+
+def filter_f():
+    user_id = users.user_id()
+    sql = text("SELECT DISTINCT P.id, P.title, P.content, U.username, P.sent_at FROM posts P, users U, followings F WHERE P.user_id IN (SELECT F.followed_id FROM followings F WHERE F.follower_id=:user_id AND F.visible=TRUE) AND P.user_id=U.id AND P.visible=TRUE ORDER BY P.sent_at DESC")
+    result = db.session.execute(sql, {"user_id":user_id})
+    return result.fetchall()
+
+def filter_l():
+    user_id = users.user_id()
+    sql = text("SELECT DISTINCT P.id, P.title, P.content, U.username, P.sent_at  FROM posts P JOIN users U ON P.user_id = U.id JOIN likes L ON P.id = L.post_id WHERE L.user_id = :user_id AND L.visible = TRUE AND P.visible = TRUE ORDER BY P.sent_at DESC")
+    result = db.session.execute(sql, {"user_id":user_id})
     return result.fetchall()
