88ab7cf30aad8e7cd670d249fccddd41c128be1c
Hanna Turunen <hannamatilda.turunen@gmail.com>
2024-09-10 16:14:02 +0300

    Deleting user sql

--- users.py
+++ users.py
@@ -5,10 +5,13 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from db import db
 
 def login(username, password):
-    sql = text("SELECT id, password FROM users WHERE username=:username")
+    sql = text("SELECT id, password, visible FROM users WHERE username=:username")
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if not user:
+        return False
+    print(user)
+    if user[2] == False:
         return False
     if check_password_hash(user.password, password):
         session["user_id"] = user.id
@@ -38,13 +41,11 @@ def user_name(user):
     return result.fetchone()
 
 def viewprofile(user):
-    #Select profile for viewing
-    sql = text("SELECT U.id, U.username FROM users U WHERE U.id=:id")
+    sql = text("SELECT U.id, U.username FROM users U WHERE U.id=:id AND U.visible=TRUE")
     result = db.session.execute(sql, {"id":user})
     return result.fetchone()
 
 def search_user(search):
-    #Search
     sql = text("""SELECT U.id, U.username, COUNT(P.id)
                 FROM posts P, users U
                 WHERE U.visible = TRUE 
@@ -55,3 +56,9 @@ def search_user(search):
                 """)
     result = db.session.execute(sql, {"search": "%"+search+"%"})
     return result.fetchall()
+
+def del_user(user):
+    sql = text("UPDATE users SET visible=FALSE WHERE id=:user_id")
+    db.session.execute(sql, {"user_id":user})
+    db.session.commit()
+    return True
