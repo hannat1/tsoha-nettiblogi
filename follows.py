1ae5de1628bfbf1950e91aacdacafa6c1468b7a6
Hanna Turunen <hannamatilda.turunen@gmail.com>
2024-09-10 16:28:59 +0300

    Delete descriptions of functions and make sure a deleted users follow doesn't show and also in filter by following deleted account doesnt show

--- follows.py
+++ follows.py
@@ -3,14 +3,13 @@ from db import db
 import users
 
 def followers(user_id):
-    #Get followers of a user
     sql = text("""SELECT U.username, U.id FROM users U, followings F
-               WHERE F.follower_id=U.id AND F.followed_id=:user_id AND F.visible=TRUE""")
+               WHERE F.follower_id=U.id AND F.followed_id=:user_id
+               AND F.visible=TRUE AND U.visible=TRUE""")
     result = db.session.execute(sql, {"user_id":user_id})
     return result.fetchall()
 
 def follow(user):
-    #Follow a profile
     user_id = users.user_id()
     if user_id == 0:
         return False
@@ -20,7 +19,6 @@ def follow(user):
     return True
 
 def unfollow(user):
-    #Unfollow a profile
     follower_id = users.user_id()
     followed_id = user
     sql = text("""UPDATE followings SET visible=FALSE
@@ -30,11 +28,10 @@ def unfollow(user):
     return True
 
 def filter_f():
-    #Filter posts by following
     user_id = users.user_id()
     sql = text("""SELECT DISTINCT P.id, P.title, P.content, U.username, P.sent_at
                FROM posts P, users U, followings F 
-               WHERE P.user_id IN (SELECT F.followed_id FROM followings F WHERE F.follower_id=:user_id AND F.visible=TRUE) 
-               AND P.user_id=U.id AND P.visible=TRUE ORDER BY P.sent_at DESC""")
+               WHERE P.user_id IN (SELECT F.followed_id FROM followings F JOIN users U ON U.id=F.followed_id WHERE F.follower_id=:user_id AND F.visible=TRUE AND U.visible=TRUE) 
+               AND P.user_id=U.id AND P.visible=TRUE AND U.visible=TRUE ORDER BY P.sent_at DESC""")
     result = db.session.execute(sql, {"user_id":user_id})
     return result.fetchall()
